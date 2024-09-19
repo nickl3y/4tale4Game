cc437a787a172959f3ff9fb663d68cfff3256950
Nickley <83526077+nickl3y@users.noreply.github.com>
2024-09-19 20:59:40 +0500

    Update1

--- Tale4Game/Intermediate/Build/Win64/x64/Tale4GameEditor/Development/Core/SharedPCH.Core.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.36.32542
-#include "C:/Users/VR NT/Documents/GitHub/4tale4Game/Tale4Game/Intermediate/Build/Win64/x64/Tale4GameEditor/Development/Core/SharedPCH.Core.Cpp20.h"
--- Tale4Game/Intermediate/Build/Win64/x64/Tale4GameEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/Users/VR NT/Documents/GitHub/4tale4Game/Tale4Game/Intermediate/Build/Win64/x64/Tale4GameEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
